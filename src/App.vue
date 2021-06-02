69da873c6fbb25f64b66a32c9e15eb91b2f418ba
Wellington Roth <wellingtonrothh@gmail.com>
2021-06-02 14:17:25 -0300

    api

--- src/App.vue
+++ src/App.vue
@@ -29,10 +29,13 @@
         </thead>
 
         <tbody>
-          <tr>
-            <td>Arduino</td>
-            <td>100</td>
-            <td>50.00</td>
+          <tr 
+            v-for="produto of produtos" 
+            :key="produto.id"
+          >
+            <td>{{ produto.nome }}</td>
+            <td>{{ produto.quantidade }}</td>
+            <td>{{ produto.valor }}</td>
             <td>
               <button class="waves-effect btn-small blue darken-1"><i class="material-icons">create</i></button>
               <button class="waves-effect btn-small red darken-1"><i class="material-icons">delete_sweep</i></button>
@@ -46,6 +49,21 @@
 </template>
 
 <script>
+import Produto from './services/produtos'
+
+export default {
+  data() {
+    return {
+      produtos: []
+    } 
+  },
+
+  mounted() {
+    Produto.listar().then(resposta => {
+      this.produtos = resposta.data
+    })
+  }
+}
 
 </script>
 
